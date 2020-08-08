2ab7a3a81a1171f33e1be25b311a2b149211c355
Aron Tolentino <arontolentino@outlook.com>
2020-08-08 17:42:55 -0400

    Increase site wrapper padding

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -6,7 +6,7 @@ import Footer from './Footer';
 const Wrapper = styled('div')`
 	max-width: ${({ fullWidth }) => (!fullWidth ? '1600px' : 'none')};
 	margin: 0 auto;
-	padding: 0 2rem;
+	padding: 0 4rem;
 `;
 
 const Layout = ({ children }) => {
