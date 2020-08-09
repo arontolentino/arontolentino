9f8494d4fd70984980572283de4ddf5319b4edc1
Aron Tolentino <arontolentino@outlook.com>
2020-08-09 16:47:19 -0400

    Create initial newsletter section on home page

--- components/home/Newsletter.js
+++ components/home/Newsletter.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
 import SectionTitle from '../layout/SectionTitle';
+import PrimaryBtn from '../layout/PrimaryBtn';
 
 const Section = styled('section')`
 	position: relative;
 	padding: 0 0 2rem 0;
+`;
+
+const NewsletterContainer = styled('div')`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin: 10rem 0;
+`;
+
+const Content = styled('div')`
+	flex: 0 0 48%;
+
+	h3 {
+		font-size: 6rem;
+	}
+
+	p {
+		font-size: 1.9rem;
+	}
+`;
+
+const Signup = styled('form')`
+	display: flex;
+	flex-direction: column;
+	flex: 0 0 48%;
+	padding: 6rem 4rem;
+	border: 0.2rem solid #000;
+
+	input {
+		margin-bottom: 3rem;
+		padding: 1rem 2rem;
+		border: none;
+		border-bottom: 0.2rem solid #000;
+	}
+`;
+
+const BtnContainer = styled('div')`
+	margin-top: 3rem;
+	display: flex;
+	justify-content: center;
 `;
 
 const Newsletter = () => {
@@ -13,6 +54,26 @@ const Newsletter = () => {
 			<SectionTitle>
 				<h2>Newsletter</h2>
 			</SectionTitle>
+
+			<NewsletterContainer>
+				<Content>
+					<h3>Follow my story from junior to senior web developer.</h3>
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry. Lorem Ipsum has been the industry's standard dummy text
+						ever since the 1500s, when an unknown printer took a galley of type
+						and scrambled it to make a type specimen book.
+					</p>
+				</Content>
+				<Signup>
+					<input type="text" placeholder="Email" />
+					<input type="text" placeholder="Name" />
+
+					<BtnContainer>
+						<PrimaryBtn>Sign Up</PrimaryBtn>
+					</BtnContainer>
+				</Signup>
+			</NewsletterContainer>
 		</Section>
 	);
 };
